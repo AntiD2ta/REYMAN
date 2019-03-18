1ff00864e3940dbd19cb7538423bdd4df4084d55
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-03-18 12:49:40 -0500

    Entities updated.
    Changes:
    -AccionConstructiva and Material entities now have a relation many to many with AccionC_Material instead of a relation many to many directly between them.
    -UnidadOrganizativa property of Usuario is set virtual.

--- BizData/Entities/AccionConstructiva.cs
+++ BizData/Entities/AccionConstructiva.cs
@@ -11,7 +11,7 @@ namespace BizData.Entities
         public string Nombre { get; set; }
 
         public virtual ObjetoObra ObjetoObra { get; set; }
-        public virtual ICollection<Material> Materiales { get; set; }
+        public virtual ICollection<AccionC_Material> Materiales { get; set; }
         public virtual Plan Plan { get; set; }
         public virtual ICollection<ManoObra> ManoObra { get; set; }
     }

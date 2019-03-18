3d0adf7798f282f68bab6d02b790e75a009e9e90
kl <k.sosa@gmail.com>
2019-03-18 15:11:16 -0700

    Prov

---
+++ BizLogic/Administration/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizLogic.Administration
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        public Vistas Vista { get; set; }
+        public ICollection<Tuple<string,string>> Claims;
+    }
+    public enum Vistas{ 
+    Plan,
+    Materiales
+    };
+}

8b2f11267ffa31a0e1475ecfe2187740fd49c643
Juan David Menendez del Cueto <juanda.mdc97@gmail.com>
2019-03-18 13:25:13 -0400

    creando acceso a la bd de provicia y unidad organizativa

---
+++ BizDbAccess/Authentication/UnidadOrganizativaDbAccess.cs
@@ -0,0 +1,47 @@
+﻿using BizData.Entities;
+using BizDbAccess.GenericInterfaces;
+using DataLayer.EfCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizDbAccess.Authentication
+{
+    public class UnidadOrganizativaDbAccess : IEntityDbAccess<UnidadOrganizativa>
+    {
+        public readonly EfCoreContext _context;
+
+        public UnidadOrganizativaDbAccess(IUnitOfWork context)
+        {
+            _context = (EfCoreContext)context;
+        }
+
+        public void Add(UnidadOrganizativa entity)
+        {
+            _context.UnidadesOrganizativas.Add(entity);
+        }
+
+        public void Delete(UnidadOrganizativa entity)
+        {
+            if (_context.UnidadesOrganizativas.Find(entity.UnidadOrganizativaID) != null)
+            {
+                _context.UnidadesOrganizativas.Remove(entity);
+                _context.Commit();
+            }
+        }
+
+        public IEnumerable<UnidadOrganizativa> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UnidadOrganizativa entity)
+        {
+            if (_context.UnidadesOrganizativas.Find(entity.UnidadOrganizativaID) != null)
+            {
+                _context.UnidadesOrganizativas.Update(entity);
+                _context.Commit();
+            }
+        }
+    }
+}

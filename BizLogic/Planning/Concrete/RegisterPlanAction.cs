b4ecd24bdbd7eb66c59ed8c66ab4eb39886b4331
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-03-19 22:44:10 -0500

    InvestorServices created and begin to be implemented. Begin of the logic of create a Plan.
    New:
    -RegisterPlan, GetPlan by year and type of plan, and UpdatePlan functionalities.
    -Is important to remark that all the features needed to this methods in the repos is implemented.

---
+++ BizLogic/Planning/Concrete/RegisterPlanAction.cs
@@ -0,0 +1,45 @@
+﻿using BizData.Entities;
+using BizDbAccess.Authentication;
+using BizDbAccess.User;
+using BizLogic.GenericInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizLogic.Planning.Concrete
+{
+    public class RegisterPlanAction : BizActionErrors, IBizAction<PlanCommand, Plan>
+    {
+        private readonly PlanDbAccess _dbAccess;
+
+        public RegisterPlanAction(PlanDbAccess dbAccess)
+        {
+            _dbAccess = dbAccess;
+        }
+
+        public Plan Action(PlanCommand dto)
+        {
+            Plan plan = dto.ToPlan();
+
+            if (plan.TipoPlan != "Reparación" &&
+                plan.TipoPlan != "Mantenimiento")
+            {
+                AddError("El tipo de Plan debe ser Mantenimiento o Reparación");
+            }
+
+            try
+            {
+                _dbAccess.GetPlan(plan.Año, plan.TipoPlan);
+            }
+            catch
+            {
+                AddError("Ya existe ese plan");
+            }
+
+            if (!HasErrors)
+                _dbAccess.Add(plan);
+
+            return HasErrors ? null : plan;
+        }
+    }
+}
---
+++ ServiceLayer/InvestorServices/InvestorServices.cs
@@ -0,0 +1,47 @@
+﻿using BizData.Entities;
+using BizDbAccess.GenericInterfaces;
+using BizDbAccess.User;
+using BizLogic.Planning;
+using BizLogic.Planning.Concrete;
+using ServiceLayer.BizRunners;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.InvestorServices
+{
+    public class InvestorServices
+    {
+        private readonly RunnerWriteDb<PlanCommand, Plan> _runnerPlan;
+        private readonly PlanDbAccess _planDbAccess;
+
+        public InvestorServices(IUnitOfWork context)
+        {
+            _runnerPlan = new RunnerWriteDb<PlanCommand, Plan>(
+                new RegisterPlanAction(new PlanDbAccess(context)), context);
+            _planDbAccess = new PlanDbAccess(context);
+        }
+
+        public long RegisterPlan(PlanCommand cmd)
+        {
+            var plan = _runnerPlan.RunAction(cmd);
+
+            if (_runnerPlan.HasErrors) return 0;
+
+            return plan.PlanID;
+        }
+
+        public Plan GetPlan(int año, string tipo)
+        {
+            return _planDbAccess.GetPlan(año, tipo);
+        }
+
+        public void UpdatePlan(Plan plan, PlanCommand cmd)
+        {
+            var upd = cmd.ToPlan();
+            upd.PlanID = plan.PlanID;
+
+            _planDbAccess.Update(upd);
+        }
+    }
+}

831cf7306c523164c749d43051206c5fb0eee231
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-03-28 23:26:34 -0500

    UnidadMedida added as a table and repo created.

---
+++ BizDbAccess/Repositories/UnidadMedidaDbAccess.cs
@@ -0,0 +1,42 @@
+﻿using BizData.Entities;
+using BizDbAccess.GenericInterfaces;
+using DataLayer.EfCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BizDbAccess.Repositories
+{
+    public class UnidadMedidaDbAccess : IEntityDbAccess<UnidadMedida>
+    {
+        private readonly EfCoreContext _context;
+
+        public UnidadMedidaDbAccess(IUnitOfWork context)
+        {
+            _context = (EfCoreContext)context;
+        }
+
+        public void Add(UnidadMedida entity)
+        {
+            _context.UnidadesMedida.Add(entity);
+        }
+
+        public void Delete(UnidadMedida entity)
+        {
+            _context.UnidadesMedida.Remove(entity);
+        }
+
+        public IEnumerable<UnidadMedida> GetAll() => _context.UnidadesMedida;
+
+        public UnidadMedida Update(UnidadMedida entity, UnidadMedida toUpd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UnidadMedida GetUM(string nombre)
+        {
+            return _context.UnidadesMedida.Where(um => um.Nombre == nombre).SingleOrDefault();
+        }
+    }
+}

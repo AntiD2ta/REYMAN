196572f43718ee07b9de3fdf95fafa8ee6bfbda3
Juan David Menendez del Cueto <juanda.mdc97@gmail.com>
2019-04-04 15:51:45 -0400

    add preciosCUP and precioCUC to ManoObra add preciosCUP, precioCUC and Cantidad to AccionC_Material

--- BizData/Entities/AccionC_Material.cs
+++ BizData/Entities/AccionC_Material.cs
@@ -7,7 +7,9 @@ namespace BizData.Entities
     public class AccionC_Material
     {
         public int AccionC_MaterialID { get; set; }
-        public Decimal Precio { get; set; }
+        public decimal PrecioCUP { get; set; }
+        public decimal PrecioCUC { get; set; }
+        public decimal Cantidad { get; set; }
 
         public virtual AccionConstructiva AccionConstructiva { get; set; }
         public virtual Material Material { get; set; }

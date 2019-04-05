7c6dc37a9a7cb6fc4aea19d07a5859282df81ee8
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-04-05 16:21:29 -0500

    Added jquery and bootstrap packages.
    Check changes in AdminService.cs

--- ServiceLayer/AdminServices/AdminService.cs
+++ ServiceLayer/AdminServices/AdminService.cs
@@ -38,6 +38,8 @@ namespace ServiceLayer.AdminServices
             return uo.UnidadOrganizativaID;
         }
 
+        //TODO: EditUO, DeleteUO
+        
         public long RegisterProvincia(ProvinciaViewModel vm)
         {
             var prov = _runnerProv.RunAction(vm);

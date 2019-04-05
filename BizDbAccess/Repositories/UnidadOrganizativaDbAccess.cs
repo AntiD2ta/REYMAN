aca6ab46e1d0aa46882532e4cac74f8fce65444f
Juan David Menendez del Cueto <juanda.mdc97@gmail.com>
2019-04-05 16:06:16 -0400

    deleted Especialidad from Unidad Organizativa

--- BizDbAccess/Repositories/UnidadOrganizativaDbAccess.cs
+++ BizDbAccess/Repositories/UnidadOrganizativaDbAccess.cs
@@ -50,11 +50,5 @@ namespace BizDbAccess.Authentication
         {
             return _context.UnidadesOrganizativas.Where(uo => uo.Nombre == nombreUO).Single();
         }
-
-        public void AddEspecialidad(ref UnidadOrganizativa entity, IEnumerable<Especialidad> especialidades)
-        {
-            entity.Especialidades = entity.Especialidades.Concat(especialidades).ToList();
-            _context.Update(entity);
-        }
     }
 }

4bb9287493ea794f7917b1db6ccd6f22827ac042
Juan David Menendez del Cueto <juanda.mdc97@gmail.com>
2019-04-06 18:37:27 -0400

    add the service to generate and export reports

---
+++ ServiceLayer/Reports/GenerateReport.cs
@@ -0,0 +1,41 @@
+﻿using BizDbAccess.GenericInterfaces;
+using BizLogic.Reports;
+using DataLayer.EfCode;
+using System.Collections.Generic;
+
+namespace ServiceLayer.Reports
+{
+    public class GenerateReport
+    {
+        private readonly EfCoreContext _context;
+
+        public GenerateReport(IUnitOfWork unitOfWork)
+        {
+            _context = (EfCoreContext)unitOfWork;
+        }
+
+        public object GenerateReport1(int year, string tipoPlan, IEnumerable<string> uos, IEnumerable<string> inmuebles)
+        {
+            GenerateReport1 report = new GenerateReport1(_context);
+            return report.GenerateReport(year, tipoPlan, uos, inmuebles);
+        }
+
+        public object GenerateReport2(int year, IEnumerable<string> uos)
+        {
+            GenerateReport2 report = new GenerateReport2(_context);
+            return report.GenerateReport(year, uos);
+        }
+
+        public object GenerateReport4(int year, IEnumerable<string> uos)
+        {
+            GenerateReport4 report = new GenerateReport4(_context);
+            return report.GenerateReport(year, uos);
+        }
+
+        public object GenerateReport5(int year, IEnumerable<string> uos)
+        {
+            GenerateReport5 report = new GenerateReport5(_context);
+            return report.GenerateReport(year, uos);
+        }
+    }
+}

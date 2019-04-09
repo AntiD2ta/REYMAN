63402e0254a1cf77acfc4591abce5416f8531afb
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-04-08 21:49:01 -0500

    Added EstadoPlan enum and some corrections to Account/Register.cshtml and User/Index.cshtml.

--- BizData/Entities/Plan.cs
+++ BizData/Entities/Plan.cs
@@ -4,12 +4,21 @@ using System.Text;
 
 namespace BizData.Entities
 {
+    public enum EstadoPlan
+    {
+        planificación,
+        aprovado,
+        ejecución,
+        ejecutado
+    }
+
     public class Plan
     {
         public int PlanID { get; set; }
         public Decimal Presupuesto { get; set; }
         public int Año { get; set; }
         public string TipoPlan { get; set; }
+        public EstadoPlan Estado { get; set; }
 
         public virtual ICollection<AccionConstructiva> AccionesConstructivas { get; set; }
     }

29375fc6e14115a67c306b26a1ec503055fc5333
Karl <karlsosa4298@gmail.com>
2019-04-12 16:33:18 -0400

    I'll go back later

--- BizLogic/Reports/ExportReport2.cs
+++ BizLogic/Reports/ExportReport2.cs
@@ -1,105 +1,105 @@
-﻿using OfficeOpenXml;
-using OfficeOpenXml.Style;
+﻿//using OfficeOpenXml;
+//using OfficeOpenXml.Style;
 
-namespace BizLogic.Reports
-{
-    public class ExportReport2
-    {
-        public byte[] Generate(dynamic report)
-        {
-            byte[] fileContents;
-            int fila = 6;
+//namespace BizLogic.Reports
+//{
+//    public class ExportReport2
+//    {
+//        public byte[] Generate(dynamic report)
+//        {
+//            byte[] fileContents;
+//            int fila = 6;
 
-            using (var package = new ExcelPackage())
-            {
-                var worksheet = package.Workbook.Worksheets.Add("Reporte 2");
+//            using (var package = new ExcelPackage())
+//            {
+//                var worksheet = package.Workbook.Worksheets.Add("Reporte 2");
 
-                //crear el "encabezado" de la tabla
-                worksheet.Cells[1, 1, 1, 6].Merge = true;
-                worksheet.Cells[1, 1].Value = "Planes de Reparación y Mantenimiento Constructivo";
+//                //crear el "encabezado" de la tabla
+//                worksheet.Cells[1, 1, 1, 6].Merge = true;
+//                worksheet.Cells[1, 1].Value = "Planes de Reparación y Mantenimiento Constructivo";
 
-                worksheet.Cells[2, 1, 2, 6].Merge = true;
-                worksheet.Cells[2, 1].Value = "Presupuesto resumen por inmuebles de la U.O.";
+//                worksheet.Cells[2, 1, 2, 6].Merge = true;
+//                worksheet.Cells[2, 1].Value = "Presupuesto resumen por inmuebles de la U.O.";
 
-                worksheet.Cells[2, 8, 2, 9].Merge = true;
-                worksheet.Cells[2, 8].Value = $"Año: {report.año}";
+//                worksheet.Cells[2, 8, 2, 9].Merge = true;
+//                worksheet.Cells[2, 8].Value = $"Año: {report.año}";
 
-                worksheet.Cells[4, 1, 5, 2].Merge = true;
-                worksheet.Cells[4, 1].Value = "Unidad Organizativa";
+//                worksheet.Cells[4, 1, 5, 2].Merge = true;
+//                worksheet.Cells[4, 1].Value = "Unidad Organizativa";
 
-                worksheet.Cells[4, 3, 5, 4].Merge = true;
-                worksheet.Cells[4, 3].Value = "Inmueble";
+//                worksheet.Cells[4, 3, 5, 4].Merge = true;
+//                worksheet.Cells[4, 3].Value = "Inmueble";
 
-                worksheet.Cells[4, 5, 4, 7].Merge = true;
-                worksheet.Cells[4, 5].Value = "Total";
-                worksheet.Cells[5, 5].Value = "MT";
-                worksheet.Cells[5, 6].Value = "CUC";
-                worksheet.Cells[5, 7].Value = "CUP";
+//                worksheet.Cells[4, 5, 4, 7].Merge = true;
+//                worksheet.Cells[4, 5].Value = "Total";
+//                worksheet.Cells[5, 5].Value = "MT";
+//                worksheet.Cells[5, 6].Value = "CUC";
+//                worksheet.Cells[5, 7].Value = "CUP";
 
-                worksheet.Cells[4, 8, 4, 10].Merge = true;
-                worksheet.Cells[4, 8].Value = "Reparaciones";
-                worksheet.Cells[5, 8].Value = "MT";
-                worksheet.Cells[5, 9].Value = "CUC";
-                worksheet.Cells[5, 10].Value = "CUP";
+//                worksheet.Cells[4, 8, 4, 10].Merge = true;
+//                worksheet.Cells[4, 8].Value = "Reparaciones";
+//                worksheet.Cells[5, 8].Value = "MT";
+//                worksheet.Cells[5, 9].Value = "CUC";
+//                worksheet.Cells[5, 10].Value = "CUP";
 
-                worksheet.Cells[4, 11, 4, 13].Merge = true;
-                worksheet.Cells[4, 11].Value = "Mantenimiento";
-                worksheet.Cells[5, 11].Value = "MT";
-                worksheet.Cells[5, 12].Value = "CUC";
-                worksheet.Cells[5, 13].Value = "CUP";
+//                worksheet.Cells[4, 11, 4, 13].Merge = true;
+//                worksheet.Cells[4, 11].Value = "Mantenimiento";
+//                worksheet.Cells[5, 11].Value = "MT";
+//                worksheet.Cells[5, 12].Value = "CUC";
+//                worksheet.Cells[5, 13].Value = "CUP";
 
-                //llenando la tabla
-                foreach (var unidad in report.unidades)
-                {
-                    worksheet.Cells[fila, 1, fila, 2].Merge = true;
-                    worksheet.Cells[fila, 1, fila, 2].Value = unidad.nombre;
-                    worksheet.Cells[fila, 1].Style.Font.Bold = true;
+//                //llenando la tabla
+//                foreach (var unidad in report.unidades)
+//                {
+//                    worksheet.Cells[fila, 1, fila, 2].Merge = true;
+//                    worksheet.Cells[fila, 1, fila, 2].Value = unidad.nombre;
+//                    worksheet.Cells[fila, 1].Style.Font.Bold = true;
 
-                    foreach (var inmueble in unidad.inmuebles)
-                    {
-                        worksheet.Cells[fila, 3, fila, 4].Merge = true;
-                        worksheet.Cells[fila, 3, fila, 4].Value = inmueble.nombre;
+//                    foreach (var inmueble in unidad.inmuebles)
+//                    {
+//                        worksheet.Cells[fila, 3, fila, 4].Merge = true;
+//                        worksheet.Cells[fila, 3, fila, 4].Value = inmueble.nombre;
 
-                        worksheet.Cells[fila, 5].Value = inmueble.reparacionesCUC + inmueble.reparacionesCUP + inmueble.mantenimientoCUC + inmueble.mantenimientoCUP;
-                        worksheet.Cells[fila, 6].Value = inmueble.reparacionesCUC + inmueble.mantenimientoCUC;
-                        worksheet.Cells[fila, 7].Value = inmueble.reparacionesCUP + inmueble.mantenimientoCUP;
-                        worksheet.Cells[fila, 8].Value = inmueble.reparacionesCUC + inmueble.reparacionesCUP;
-                        worksheet.Cells[fila, 9].Value = inmueble.reparacionesCUC;
-                        worksheet.Cells[fila, 10].Value = inmueble.reparacionesCUP;
-                        worksheet.Cells[fila, 11].Value = inmueble.mantenimientoCUC + inmueble.mantenimientoCUP;
-                        worksheet.Cells[fila, 12].Value = inmueble.mantenimientoCUC;
-                        worksheet.Cells[fila, 13].Value = inmueble.mantenimientoCUP;
+//                        worksheet.Cells[fila, 5].Value = inmueble.reparacionesCUC + inmueble.reparacionesCUP + inmueble.mantenimientoCUC + inmueble.mantenimientoCUP;
+//                        worksheet.Cells[fila, 6].Value = inmueble.reparacionesCUC + inmueble.mantenimientoCUC;
+//                        worksheet.Cells[fila, 7].Value = inmueble.reparacionesCUP + inmueble.mantenimientoCUP;
+//                        worksheet.Cells[fila, 8].Value = inmueble.reparacionesCUC + inmueble.reparacionesCUP;
+//                        worksheet.Cells[fila, 9].Value = inmueble.reparacionesCUC;
+//                        worksheet.Cells[fila, 10].Value = inmueble.reparacionesCUP;
+//                        worksheet.Cells[fila, 11].Value = inmueble.mantenimientoCUC + inmueble.mantenimientoCUP;
+//                        worksheet.Cells[fila, 12].Value = inmueble.mantenimientoCUC;
+//                        worksheet.Cells[fila, 13].Value = inmueble.mantenimientoCUP;
 
-                        ++fila;
-                    }
+//                        ++fila;
+//                    }
 
-                    worksheet.Cells[fila, 3, fila, 4].Merge = true;
-                    worksheet.Cells[fila, 3, fila, 13].Style.Border.Top.Style = ExcelBorderStyle.Thin;
-                    worksheet.Cells[fila, 1, fila, 13].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-                    worksheet.Cells[fila, 3, fila, 13].Style.Font.Bold = true;
-                    worksheet.Cells[fila, 3, fila, 4].Value = "Total de la UO"; 
+//                    worksheet.Cells[fila, 3, fila, 4].Merge = true;
+//                    worksheet.Cells[fila, 3, fila, 13].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+//                    worksheet.Cells[fila, 1, fila, 13].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+//                    worksheet.Cells[fila, 3, fila, 13].Style.Font.Bold = true;
+//                    worksheet.Cells[fila, 3, fila, 4].Value = "Total de la UO"; 
 
-                    worksheet.Cells[fila, 5].Value = unidad.reparacionesCUC + unidad.reparacionesCUP + unidad.mantenimientoCUC + unidad.mantenimientoCUP;
-                    worksheet.Cells[fila, 6].Value = unidad.reparacionesCUC + unidad.mantenimientoCUC;
-                    worksheet.Cells[fila, 7].Value = unidad.reparacionesCUP + unidad.mantenimientoCUP;
-                    worksheet.Cells[fila, 8].Value = unidad.reparacionesCUC + unidad.reparacionesCUP;
-                    worksheet.Cells[fila, 9].Value = unidad.reparacionesCUC;
-                    worksheet.Cells[fila, 10].Value = unidad.reparacionesCUP;
-                    worksheet.Cells[fila, 11].Value = unidad.mantenimientoCUC + unidad.mantenimientoCUP;
-                    worksheet.Cells[fila, 12].Value = unidad.mantenimientoCUC;
-                    worksheet.Cells[fila, 13].Value = unidad.mantenimientoCUP;
+//                    worksheet.Cells[fila, 5].Value = unidad.reparacionesCUC + unidad.reparacionesCUP + unidad.mantenimientoCUC + unidad.mantenimientoCUP;
+//                    worksheet.Cells[fila, 6].Value = unidad.reparacionesCUC + unidad.mantenimientoCUC;
+//                    worksheet.Cells[fila, 7].Value = unidad.reparacionesCUP + unidad.mantenimientoCUP;
+//                    worksheet.Cells[fila, 8].Value = unidad.reparacionesCUC + unidad.reparacionesCUP;
+//                    worksheet.Cells[fila, 9].Value = unidad.reparacionesCUC;
+//                    worksheet.Cells[fila, 10].Value = unidad.reparacionesCUP;
+//                    worksheet.Cells[fila, 11].Value = unidad.mantenimientoCUC + unidad.mantenimientoCUP;
+//                    worksheet.Cells[fila, 12].Value = unidad.mantenimientoCUC;
+//                    worksheet.Cells[fila, 13].Value = unidad.mantenimientoCUP;
 
-                    ++fila;
-                }
+//                    ++fila;
+//                }
 
-                worksheet.Cells[5, 1, 5, 13].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-                worksheet.Cells[1, 1, 5, 13].Style.Font.Bold = true;
-                worksheet.Cells[1, 1, fila, 13].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                worksheet.Cells[1, 1, fila, 13].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
-                fileContents = package.GetAsByteArray();
-            }
+//                worksheet.Cells[5, 1, 5, 13].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+//                worksheet.Cells[1, 1, 5, 13].Style.Font.Bold = true;
+//                worksheet.Cells[1, 1, fila, 13].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+//                worksheet.Cells[1, 1, fila, 13].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+//                fileContents = package.GetAsByteArray();
+//            }
 
-            return fileContents;
-        }
-    }
-}
+//            return fileContents;
+//        }
+//    }
+//}

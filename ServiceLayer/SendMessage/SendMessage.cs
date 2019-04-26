c33d6659a130a1d99a8e83fa31d3bacb8fd6836d
Karl <karlsosa4298@gmail.com>
2019-04-26 16:58:00 -0400

    last changes

--- ServiceLayer/SendMessage/SendMessage.cs
+++ ServiceLayer/SendMessage/SendMessage.cs
@@ -10,22 +10,22 @@ namespace ServiceLayer.SendMessage
     {
         public void Send(string firstName, string secondName, string firstLastName, string secondLastName, string email)
         {
-            //var message = new MimeMessage();
-            //message.From.Add(new MailboxAddress("reymanproject@gmail.com"));
-            //message.To.Add(new MailboxAddress("juanda.mdc97@gmail.com"));
-            //message.Subject = "Nuevo usuario";
-            //message.Body = new TextPart("plain")
-            //{
-            //    Text = $"El usuario {firstName + secondLastName + firstLastName + secondLastName}, con email {email}, esta pendiente de autorizacion."
-            //};
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("reymanproject@gmail.com"));
+            message.To.Add(new MailboxAddress("juanda.mdc97@gmail.com"));
+            message.Subject = "Nuevo usuario";
+            message.Body = new TextPart("plain")
+            {
+                Text = $"El usuario {firstName + secondLastName + firstLastName + secondLastName}, con email {email}, esta pendiente de autorizacion."
+            };
 
-            //using (var client = new SmtpClient())
-            //{
-            //    client.Connect("localhost", 587, false);
-            //    //client.Authenticate("reymanproject@gmail.com", "REYMAN.2019");
-            //    client.Send(message);
-            //    client.Disconnect(true);
-            //}
+            using (var client = new SmtpClient())
+            {
+                client.Connect("localhost", 587, false);
+                //client.Authenticate("reymanproject@gmail.com", "REYMAN.2019");
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
     }
 }

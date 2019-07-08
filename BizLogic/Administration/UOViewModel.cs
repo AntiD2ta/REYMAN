461deee7e0176228f67c259d8d9e3d59707a9aae
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-07-07 19:19:05 -0500

    TODOs updated.

--- BizLogic/Administration/UOViewModel.cs
+++ BizLogic/Administration/UOViewModel.cs
@@ -8,11 +8,11 @@ namespace BizLogic.Administration
 {
     public class UOViewModel
     {
-        [Required(ErrorMessage = "El nombre de la Unidad Organizativa es necesario."]
+        [Required(ErrorMessage = "El nombre de la Unidad Organizativa es necesario.")]
         [StringLength(100)]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "La Provincia es necesaria."]
+        [Required(ErrorMessage = "La Provincia es necesaria.")]
         [StringLength(100)]
         public string Provincia { get; set; }
 

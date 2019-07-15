fe62548ad842d9beff4558048ce9e3b010c584e8
TNO <m.tenorio@estudiantes.matcom.uh.cu>
2019-07-15 15:01:22 -0500

    Configure to delete cascade Plan, and TODO updated.

--- DataLayer/EfCode/EfCoreContext.cs
+++ DataLayer/EfCode/EfCoreContext.cs
@@ -66,7 +66,7 @@ namespace DataLayer.EfCode
                .HasMany(plan => plan.AccionesConstructivas)
                .WithOne(ac => ac.Plan)
                .IsRequired()
-               .OnDelete(DeleteBehavior.Restrict);
+               .OnDelete(DeleteBehavior.Cascade);
 
             builder.Entity<AccionC_Material>()
                 .HasOne(acm => acm.AccionConstructiva)
